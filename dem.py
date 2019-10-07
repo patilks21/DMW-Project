550477b1becc71346654039ae8433c42763a5816
patilks21 <46299620+patilks21@users.noreply.github.com>
2019-10-07 08:49:36 +0530

    Update dem.py

--- dem.py
+++ dem.py
@@ -2,7 +2,7 @@ from KNN import knn
 from RandomForest import randomforest
 from SupportVector import supportvector
 knn=knn()
-'''
+b=knn.info()
 a=knn.kn()
 rf=randomforest()
 b=rf.rf()
@@ -11,5 +11,5 @@ c=sv.svc()
 print("Accuracy of KNN is : "+str(a))
 print("Accuracy of RandomForest is : "+str(b))
 print("Accuracy of SupportVector is : "+str(c))
-'''
-a=knn.info()
+
+
